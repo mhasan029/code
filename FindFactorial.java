e618bb5e853c74b6ff0e4231c433eb4d71b4bee5
unknown <mhasan.sust@gmail.com>
2021-10-09 15:08:15 +0600

    FindFactorial java code added in main branch

---
+++ FindFactorial.java
@@ -0,0 +1,25 @@
+package javaproject;
+
+class MyClass {
+	   int num;
+	   
+	   MyClass(int n ) {
+	      num = n;
+	   }
+	}
+
+public class FindFactorial {
+
+	public static void main(String[] args) {
+		
+		MyClass MyObj = new MyClass(4);
+		int fact =1;
+		for (int i=1; i<=MyObj.num;i++) {
+			fact = fact*i;
+		}
+		System.out.println("Factorial of " + MyObj.num + " is: " + fact);
+		
+
+	}
+
+}
